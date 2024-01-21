3696b720f39bc4069c2079ff2b327da2e0c38958
cedric hamilton <cedricjhamilton@gmail.com>
2024-01-21 16:20:14 -0500

    updated index js

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ReadMe Generator
 
 ## Name
-
+README Generator
 ## Description
-
+Created a professional README Generator that generates a README file outlining the key features of user's project using command-line prompts.  
 ## Links
 
 ## Usage
-
+Created to recieve prompted inputs through the command-line and output the responses into a display with details.
 ## Walk Thru Video
--- index.js
+++ index.js
@@ -8,8 +8,89 @@ const outputREADME = (readMe) => {
         console.log('Success');
     })
 };
-const questions = [
-    {
+
+// ACTION
+const renderTitle = (title, license) => {
+    return `#${title} ![GitHublicense](http://img.shields.io/badge/license-${license}-blue.svg)`
+};
+
+const renderDescription =(description) => {
+    return `## Description;
+    ${description}`
+};
+
+const renderTableofcontents =() => {
+    return `##Table of Contents
+    [Instructions](#instructions)
+
+    [Usage](#usage)
+
+    [Contributing](#contributions)
+
+    [Test](#test)
+
+    [License](#license)
+
+    [GitHub](#gitHub)`
+};
+
+const renderinstallation = (installation) => {
+    return `## Installation
+    ${installation}`
+};
+
+const renderInstructions = (instructions) => {
+    return `## Instructions
+    ${instructions}`
+};
+
+const renderUsage = (usage) => {
+    return `## Usage
+    ${usage}`
+};
+
+const renderContributing = (contributing) => {
+    return `## Contributing
+    ${contributing}`
+};
+
+const renderTest = (test) => {
+    return `## Test
+    ${test}`
+};
+
+const renderLicense = (license) => {
+    return `## License
+    ![GitHub license](http://img.shields.io/badge/license-${license}-blue.svg)`
+};
+
+const renderGitHub = (gitHub) => {
+    return `## GitHub Username
+    ${gitHub}`
+};
+
+const renderBaseTemplate = (
+    titleTemplate,
+    decriptionTemplate,
+    TableofcontentsTemplate,
+    installationtemplate,
+    usageTemplate,
+    contributingTemplate,
+    testTemplate,
+    licenseTemplate,
+    gitHubTemplate,
+) => {
+    return `${titleTemplate}
+    ${descriptionTemplate}
+    ${TableofcontentsTemplate}
+    ${installationTemplate}
+    ${instructionsTemplate}
+    ${usageTemplate}
+    ${contributingTemplate}
+    ${testTemplate}
+    ${gitHubTemplate}`
+};
+var questions = [{
         type: 'input',
         message: 'What is the title of your project?',
         name: 'title',
@@ -35,118 +116,145 @@ const questions = [
             }
         }
     },
-    {type: 'input',
-    message: 'What is the installation process for your project?',
-    name: 'installation',
-    validate: your_installation => {
-        if (your_installation) {
-            return true;
-        } else {
-            console.log('Provide a installation process for your project');
-            return false;
+    {
+        type: 'input',
+        message: 'What is the installation process for your project?',
+        name: 'installation',
+        validate: your_installation => {
+            if (your_installation) {
+                return true;
+            } else {
+                console.log('Provide a installation process for your project');
+                return false;
+            }
         }
-    }
     },
-    {type: 'input',
-    message: 'How is your project used?',
-    name: 'usage',
-    validate: your_usage => {
-        if (your_usage) {
-            return true;
-        } else {
-            console.log('Provide the usage of your project');
-            return false;
+    {
+        type: 'input',
+        message: 'How is your project used?',
+        name: 'usage',
+        validate: your_usage => {
+            if (your_usage) {
+                return true;
+            } else {
+                console.log('Provide the usage of your project');
+                return false;
+            }
         }
-    }
     },
-    {type: 'input',
-    message: 'What is the contributions for your project?',
-    name: 'contributing',
-    validate: your_usage => {
-        if (your_usage) {
-            return true;
-        } else {
-            console.log('Provide the contributions for your project');
-            return false;
+    {
+        type: 'input',
+        message: 'What is the contributions for your project?',
+        name: 'contributing',
+        validate: your_contributions => {
+            if (your_contributions) {
+                return true;
+            } else {
+                console.log('Provide the contributions for your project');
+                return false;
+            }
         }
-    }
     },
-{
-    type: 'input',
+    {
+        type: 'input',
         message: 'How would you describe your project?',
-            name: 'description',
-                validate: your_description => {
-                    if (your_description) {
-                        return true;
-                    } else {
-                        console.log('Provide a description for your project');
-                        return false;
-                    }
-                }
-},
-{
-    type: 'input',
+        name: 'description',
+        validate: your_description => {
+            if (your_description) {
+                return true;
+            } else {
+                console.log('Provide a description for your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
         message: 'where are the table of contents for your project?',
-            name: 'tableofcontents',
-                validate: your_tableofcontents => {
-                    if (your_tableofcontents) {
-                        return true;
-                    } else {
-                        console.log('Provide a tableofcontents for your project');
-                        return false;
-                    }
-                }
-},
-{
-    type: 'checkbox',
+        name: 'tableofcontents',
+        validate: your_tableofcontents => {
+            if (your_tableofcontents) {
+                return true;
+            } else {
+                console.log('Provide a tableofcontents for your project');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'checkbox',
         message: 'Which license is your project covered under?',
-            name: 'license',
-                choices: [
-                    'BSD',
-                    'MIT',
-                    'GPL',
-                    'ISC',
-                ],
-                    validate: your_license => {
-                        if (your_license) {
-                            return true;
-                        } else {
-                            console.log('Select a license for your project');
-                            return false;
-                        }
-                    }
-},
-{
-    type: 'input',
-    message: 'What is your Github username?',
-    name: 'github',
-    validate: your_github => {
-        if (your_github) {
-            return true;
-        } else {
-            console.log('Provide your github username');
-            return false;
+        name: 'license',
+        choices: [
+            'BSD',
+            'MIT',
+            'GPL',
+            'ISC',
+        ],
+        validate: your_license => {
+            if (your_license) {
+                return true;
+            } else {
+                console.log('Select a license for your project');
+                return false;
+            }
         }
-    }
-},
-{
-    type: 'input',
-    message: 'What is your email address?',
-    name: 'email',
-    validate: your_email => {
-        if (your_email) {
-            return true;
-        } else {
-            console.log('Provide your email address');
-            return false;
+    },
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name: 'github',
+        validate: your_github => {
+            if (your_github) {
+                return true;
+            } else {
+                console.log('Provide your github username');
+                return false;
+            }
         }
-    }
-},
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name: 'email',
+        validate: your_email => {
+            if (your_email) {
+                return true;
+            } else {
+                console.log('Provide your email address');
+                return false;
+            }
+        }
+    },
 ];
+inquirer.prompt(questions)
+.then(({title, description, installation, instructions, usage,contributing,test, license, gitHub}) =>{
+    
+    const titleTemplate = renderTitle(title, license);
+    const descriptionTemplate = renderDescription(description);
+    const TableofcontentsTemplate = renderTableofcontents();
+    const installationTemplate = renderinstallation(installation);
+    const instructionsTemplate = renderInstructions(instructions);
+    const usageTemplate = renderUsage(usage);
+    const contributingTemplate = renderContributing(contributing);
+    const testTemplate = renderTest(test);
+    const licenseTemplate = renderLicense(license);
+    const gitHubTemplate =renderGitHub(gitHub);
+    const baseTemplate = renderBaseTemplate(
+        titleTemplate,
+        descriptionTemplate,
+        TableofcontentsTemplate,
+        installationTemplate,
+        instructionsTemplate,
+        usageTemplate,
+        contributingTemplate,
+        testTemplate,
+        licenseTemplate,
+        gitHubTemplate,
+    );
+    outputREADME(baseTemplate);
+
+})
 
 
-// TODO: Create a function to initialize app
-function init() { }
 
-// Function call to initialize app
-outputREADME()
